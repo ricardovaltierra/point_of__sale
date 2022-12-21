13e3d6a47403475813e5e48bea9d8d85137cb54d
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-20 18:58:58 -0600

    Run migrations and add factories for addresses

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,16 @@
 
 FactoryBot.define do
+  factory :address_owner do
+    owner_type { false }
+  end
+
+  factory :address do
+    postal_code { 1 }
+    street { "MyString" }
+    external_number { 1 }
+    iternal_number { "MyString" }
+  end
+
   factory :user do
     first_name { "MyString" }
     last_name { "MyString" }
