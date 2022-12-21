2d760cf8fdaba5ad3450440afad9078b8f9d8d49
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-20 18:56:20 -0600

    Add User model relationships and spec case

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  belongs_to :address_owner
+  has_one :address, through: :address_owner
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do 
+    it 'belongs to an address_owner' do
+      expect(subject).to belong_to(:address_owner)
+    end
+  end
 end
