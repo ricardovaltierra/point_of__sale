c63a04b7b1179ffe316e258ca039d16616fdcf7f
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-21 17:32:52 -0600

    Change association spec structure

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Address, type: :model do
   describe 'associations' do
-    it { should belong_to(:address_owner).class_name('AddressOwner') }
+    it 'belongs to an address_owner' do
+      is_expected.to belong_to(:address_owner)
+    end
   end
 end
