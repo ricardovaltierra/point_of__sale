cfa4461456952282395d5252df66492eb03d99cd
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-30 06:06:49 -0600

    Set migratio and update model for non-integer PK

--- app/models/allergene.rb
+++ app/models/allergene.rb
@@ -1,2 +1,3 @@
 class Allergene < ApplicationRecord
+  self.primary_key = :id
 end
--- db/migrate/20221230114343_set_non_integer_pk_on_allergenes.rb
+++ db/migrate/20221230114343_set_non_integer_pk_on_allergenes.rb
@@ -1,4 +1,13 @@
 class SetNonIntegerPkOnAllergenes < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    remove_column :allergenes, :id
+    add_column :allergenes, :id, :string
+    execute "ALTER TABLE allergenes ADD PRIMARY KEY (id);"
+  end
+
+  def down
+    execute "ALTER TABLE allergenes DROP CONSTRAINT id;"
+    rename_column :allergenes, :id, :code
+    add_column :allergenes, :id, :primary_key
   end
 end
