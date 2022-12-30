3db9af83ab08da336661a177075a4409adff2236
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-30 05:25:36 -0600

    Set allergene default value spec and migration

---
+++ db/migrate/20221230112433_set_contains_column_for_array_type.rb
@@ -0,0 +1,4 @@
+class SetContainsColumnForArrayType < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- spec/models/allergene_spec.rb
+++ spec/models/allergene_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Allergene, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'default values' do
+    it 'has contains as an empty array' do
+      expect(subject.contains).to eq []
+    end
+  end
 end
