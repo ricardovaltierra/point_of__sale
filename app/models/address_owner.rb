26bacc6fdf2bc32054acc929c8c26d916419212c
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-30 04:05:59 -0600

    Set inverse_of option on address_owner and store association spec

--- app/models/address_owner.rb
+++ app/models/address_owner.rb
@@ -1,5 +1,5 @@
 class AddressOwner < ApplicationRecord
   has_one :user, inverse_of: :address_owner
-  has_one :store
+  has_one :store, inverse_of: :address_owner
   has_one :address, inverse_of: :address_owner
 end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Store, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'belongs to an address_owner' do
+      is_expected.to belong_to(:address_owner)
+    end
+  end
 end
