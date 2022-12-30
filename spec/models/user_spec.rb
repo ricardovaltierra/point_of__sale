9a162ded7908dfe5d5a1ece25b1cc5f537121b60
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-30 00:43:59 -0600

    Add association test for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'associations' do
+    it 'belongs to an address_owner' do
+      is_expected.to belong_to(:address_owner)
+    end
+
+    it 'has one addres through address_owner' do
+      is_expected.to have_one(:address).through :address_owner
+    end
   end
 end
