fac7c42d2bbdac28f1b652906278a8d7322b47bc
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-30 03:41:14 -0600

    Update schema and fix association test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_23_200027) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_30_093509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_23_200027) do
     t.datetime "updated_at", null: false
     t.bigint "address_owner_id", null: false
     t.index ["address_owner_id"], name: "index_addresses_on_address_owner_id"
+  end
+
+  create_table "credit_cards", force: :cascade do |t|
+    t.string "name_on_card"
+    t.string "number"
+    t.string "expiration_date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_credit_cards_on_user_id"
   end
 
   create_table "stores", force: :cascade do |t|
@@ -50,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_23_200027) do
   end
 
   add_foreign_key "addresses", "address_owners"
+  add_foreign_key "credit_cards", "users"
   add_foreign_key "stores", "address_owners"
   add_foreign_key "users", "address_owners"
 end
