4283cc0b806e569691cbf0c0ddf1ee937b59f30d
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-30 05:28:05 -0600

    Set migration for default array value and update schema

--- db/migrate/20221230112433_set_contains_column_for_array_type.rb
+++ db/migrate/20221230112433_set_contains_column_for_array_type.rb
@@ -1,4 +1,5 @@
 class SetContainsColumnForArrayType < ActiveRecord::Migration[7.0]
   def change
+    change_column :allergenes, :contains, :text, array: true, default: [], using: "(string_to_array(contains, ','))"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_30_111446) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_30_112433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_30_111446) do
   end
 
   create_table "allergenes", force: :cascade do |t|
-    t.text "contains"
+    t.text "contains", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
