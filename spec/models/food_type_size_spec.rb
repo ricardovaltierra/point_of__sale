1fe3ce20c803ea29746c6ca5531f563719bce345
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-30 07:51:34 -0600

    Add association on food type and inverse_of option

--- spec/models/food_type_size_spec.rb
+++ spec/models/food_type_size_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe FoodTypeSize, type: :model do
   describe 'associations' do
     it 'belongs to a food type' do
-      is_expected.to belong_to(:food_type)
+      is_expected.to belong_to(:food_type).inverse_of :food_type_sizes
     end
   end
 end
