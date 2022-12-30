f603c2aa97067029a2348d5a86d577c6e1e5147c
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-30 07:53:44 -0600

    Update food type spec on food type sizes

--- spec/models/food_type_spec.rb
+++ spec/models/food_type_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe FoodType, type: :model do
     it 'has many food item crusts' do 
       is_expected.to have_many(:food_item_crust).inverse_of :food_type
     end
+
+    it 'has many food type sizes' do
+      is_expected.to have_many(:food_type_sizes).inverse_of :food_type
+    end
   end
 end
