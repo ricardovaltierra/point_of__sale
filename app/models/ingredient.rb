f65dd495c444074e510116e91f362fc46983efda
ricardovaltierra <ricardo_valtierra@outlook.com>
2023-01-02 13:38:55 -0600

    Add association spec for nut value on ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,5 @@
 class Ingredient < ApplicationRecord
+  has_one :nutritional_value
   belongs_to :ingredient_type, primary_key: :id,
                                inverse_of: :ingredients
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Ingredient, type: :model do
       is_expected.to belong_to(:ingredient_type).with_primary_key(:id)
                                                 .inverse_of(:ingredients)
     end
+
+    it 'has one nutritional value' do
+      is_expected.to have_one(:nutritional_value)
+    end
   end
 end
