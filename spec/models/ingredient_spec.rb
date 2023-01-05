1a78e8c32af2b94f7c9ffa44bb1cd5b6c976cf57
ricardovaltierra <ricardo_valtierra@outlook.com>
2023-01-04 19:03:13 -0600

    Set default value spec for ingredient

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe Ingredient, type: :model do
       is_expected.to have_one(:nutritional_value).inverse_of(:ingredient)
     end
   end
+
+  describe 'default values' do
+    let(:example_ingredient_type) { create :ingredient_type  }
+    let(:example_ingredient)      { create :ingredient, ingredient_type: example_ingredient_type }
+
+    it 'has a allergenes_and_additives field of array type' do
+      puts "INGREDIENT TYPE: #{example_ingredient}"
+      expect(example_ingredient.allergenes_and_additives).to be_an_instance_of Array
+    end
+  end
 end
