e8cbd92ecf7a79c5fdeb33b2eb73fc669540f09d
ricardovaltierra <ricardo_valtierra@outlook.com>
2023-01-20 22:21:03 -0600

    Temporary changing tokens and images to same one

--- db/seeds/food_items.rb
+++ db/seeds/food_items.rb
@@ -5,397 +5,397 @@ PICTURE_POSTFIX = ".jpg?alt=media&token"
 food_item_fields = [
 	{ # Begins pasta items (food type 1)
 		name: "Lunch Pasta Verduro",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 9.99
 	},
 	{ 
 		name: "Pasta Verduro",
-		token: "acbf285b-d968-4795-98e8-08f659aae498",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 9.99
 	},
 	{ 
 		name: "Pasta Cheesyo",
-		token: "c6f67d0a-072e-4efe-b2c6-ac5107b6a3a7",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 9.49
 	},
 	{ 
 		name: "Pasta Bolo",
-		token: "f3ff0898-27da-43d6-9881-8e1f53c08b23",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 10.49
 	},
 	{ 
 		name: "Pasta Curryo",
-		token: "72105ca5-2036-48e1-954b-852fcb123930",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 10.99
 	},
 	{ 
 		name: "Pasta Orlando",
-		token: "be836dc4-eb80-415b-b6de-56140afadb43",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 9.99
 	},
 	{ 
 		name: "Pasta Chicko",
-		token: "d0f62292-6f61-4b71-9216-a66090b236cb",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 9.99
 	},
 	{ 
 		name: "Pasta Steako",
-		token: "bd14646d-668c-42bf-954e-eef1edc9ee7f",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 8.59
 	},
 	{ # Begins pizza items (food type 2)
 		name: "Especially Popular - Brocoli + Hollandaise",
-		token: "4d1e36f3-fa0d-4db2-8512-77956f7a4545",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.99
 	},
 	{ 
 		name: "Lunch Pizza Individuell",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.99
 	},
 	{ 
 		name: "Popular - Checken Breast + Red Onions",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.99
 	},
 	{ 
 		name: "Popular - Minced Beef + Sweetcorn",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.99
 	},
 	{ 
 		name: "Popular - Bacon + Broccoli",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.99
 	},
 	{ 
 		name: "Popular - Mushrooms + Red Onions",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.99
 	},
 	{ 
 		name: "Dutcham",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.49
 	},
 	{ 
 		name: "Danish Burger Pizza",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.99
 	},
 	{ 
 		name: "Cheeseburger Pizza",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 11.49
 	},
 	{ 
 		name: "Teriyaki Chicken",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 13.99
 	},
 	{ 
 		name: "Crazy Dog",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.49
 	},
 	{ 
 		name: "Sucuk Hollandaise",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 13.99
 	},
 	{ 
 		name: "Bombay",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.49
 	},
 	{ 
 		name: "Boston",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.49
 	},
 	{ 
 		name: "BBQ Poncho",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 13.99
 	},
 	{ 
 		name: "Salami Supreme",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.49
 	},
 	{ 
 		name: "Lucifer",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.49
 	},
 	{ 
 		name: "Conchita",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 14.49
 	},
 	{ 
 		name: "Lucca",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.49
 	},
 	{ 
 		name: "Beef & Veggies",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 14.99
 	},
 	{ 
 		name: "Steak & Bacon",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 14.49
 	},
 	{ 
 		name: "Fitness Fan",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.99
 	},
 	{ 
 		name: "Salami",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 11.99
 	},
 	{ 
 		name: "Margherita",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 11.49
 	},
 	{ 
 		name: "Funghi",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 11.49
 	},
 	{ 
 		name: "Caprese",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 11.49
 	},
 	{ 
 		name: "Prosciutto e Funghi",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 11.99
 	},
 	{ 
 		name: "Tuna",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.99
 	},
 	{ 
 		name: "Waikiki",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.49
 	},
 	{ 
 		name: "Green Garden",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.49
 	},
 	{ 
 		name: "Las Vega",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.99
 	},
 	{ 
 		name: "Vegayaki",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 13.99
 	},
 	{ 
 		name: "Vegan Curry",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 13.99
 	},
 	{ 
 		name: "Kap Verde (Vegan)",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 12.49
 	},
 	{ 
 		name: "Create Your Own Pizza",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 9.99
 	},
 	{ 
 		name: "Vegan - Create Your Own Pizza",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 8.99
 	},
 	{ 
 		name: "Chicken Kickers",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 5.99
 	},
 	{ 
 		name: "Chicken Strippers",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 5.99
 	},
 	{ 
 		name: "Sour Cream",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 1.29
 	},
 	{ 
 		name: "Aioli",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 1.29
 	},
 	{ 
 		name: "Herb Tartar Dip",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 1.29
 	},
 	{ 
 		name: "Garlic Dip",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 1.29
 	},
 	{ 
 		name: "BBQ-Honey",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 1.29
 	},
 	{ 
 		name: "Sweet-Chili",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 1.29
 	},
 	{ 
 		name: "Calzini Salami",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Calzini Ham And Peppers",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Calzini Veggie",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Unfilled Pizza Rolls",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 4.49
 	},
 	{ 
 		name: "Las Vega Pizza Rolls",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 5.99
 	},
 	{ 
 		name: "Pizza Rolls Prosciutto Cotto and Cheese",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 5.99
 	},
 	{ 
 		name: "Pizza Rolls with Cheese",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 5.99
 	},
 	{ 
 		name: "Pizza Rolls Spinach and Mozzarella",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 5.99
 	},
 	{ 
 		name: "Pizza Rolls Sujuk and Mozzarella",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 5.99
 	},
 	{ 
 		name: "Pizza Rolls Chili Salami, Jalapeños and Cheese",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 5.99
 	},
 	{ 
 		name: "Pizza Rolls Tuna and Cheese",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 5.99
 	},
 	{ 
 		name: "Pizza Rolls Salami and Cheese",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 5.99
 	},
 	{ 
 		name: "Pizza Rolls with Vegan Grated Melt",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 5.99
 	},
 	{ 
 		name: "Wrap Veggie",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Wrap Hot Stuff",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Wrap Classic Chicken",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Wrap Italian Chicken",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Röstisticks approx. 120g",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Apple Compote",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Cheese Bites",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Lunch Salad Deluxe",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Ben & Jerry's Cookie Dough Peaces",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Ben & Jerry's Peanut Butter",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "American Dressing",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 6.49
 	},
 	{ 
 		name: "Balsamic Dressing",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 0.99
 	},
 	{ 
 		name: "Caesar's Dressing",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 0.99
 	},
 	{ 
 		name: "Yogurt Lime Dressing",
-		token: "2854d8bb-55e1-4b0e-b598-81474b45a299",
+		token: "a3e1e6c0-ce43-423e-992e-a5f3ef6ff705",
 		price: 0.99
 	}
 ]
@@ -432,7 +432,7 @@ food_item_fields.each_with_index do |item, index|
 FoodItem.create(
 	name: 			 				 					 item[:name],
 	entry_price: 				 					 item[:entry_price],
-	picture_url: 				 					 "#{URL_PREFIX||"URL_NOT_FOUND"}#{PICTURE_PREFIX}#{index + 1}#{PICTURE_POSTFIX}=#{item[:token]}",
+	picture_url: 				 					 "#{URL_PREFIX||"URL_NOT_FOUND"}#{PICTURE_PREFIX}#{14}#{PICTURE_POSTFIX}=#{item[:token]}",
 	general_description: 					 Faker::Lorem.paragraph_by_chars,
 	food_type_id: 			 					 type_id,
 	additional_information_badges: AdditionalInformationBadge.all.ids.sample(rand(6))
