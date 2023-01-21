db0ef5ed26a1cf06778e649d73c707704ac716e2
ricardovaltierra <ricardo_valtierra@outlook.com>
2023-01-20 23:05:24 -0600

    Add functionality to index and show actions

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::StoresController < ApplicationController
   def index
+    stores = Store.all
+    render json: stores, status: 200
   end
 
   def show
+    store = Store.find_by(id: params[:id])
+    if store
+      render json: store, status: 200
+    else
+      render json: { error: "Prodcut not found. Please try with another one." }
+    end
   end
 end
