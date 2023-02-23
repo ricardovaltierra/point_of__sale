4bf97262cbcbdbd303b38c61bcb28a6b6c664327
ricardovaltierra <ricardo_valtierra@outlook.com>
2023-02-23 13:31:03 -0600

    Update routes file for new controller and compact file lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'users/index'
-      get 'users/show'
-      resources :stores, only: [:index, :show, :create]
+      resources :additional_information_badges, only: [:index, :show]
+      resources :stores, only: [:index, :show]
+      resources :users, only: [:index, :show]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
