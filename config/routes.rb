e065aaa2f4105bdf721897e0b3b0a9fcbd384cdf
ricardovaltierra <ricardo_valtierra@outlook.com>
2023-02-23 12:28:06 -0600

    Add users controller with GET actions and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'users/index'
+      get 'users/show'
       resources :stores, only: [:index, :show, :create]
     end
   end
